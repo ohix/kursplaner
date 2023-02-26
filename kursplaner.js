f451957cc891744c3407a0b245ceb80f1a7a498c
Lauri0604 <Laurenz.Karrasch@gmail.com>
2023-02-26 23:33:22 +0100

    finished button added, rule 1 reworked

--- kursplaner.js
+++ kursplaner.js
@@ -6,6 +6,7 @@ const faecher12 = ['Englisch', 'Latein', 'Spanisch', 'Japanisch', 'Musik'];
 let faecher12_0_5 = faecher12.slice(0, 5);
 
 const buttons = document.querySelectorAll('.subject-button');
+const inputFinishedButton = document.getElementById('input-finished');
 
 // Add event listeners to the buttons for each subject
 buttons.forEach((button, index) => {
@@ -14,10 +15,10 @@ buttons.forEach((button, index) => {
         faecher11.push(subject);
         console.log(faecher11);
         button.disabled = true;
-        if (index == 4) {
-            faecher11_0_5 = faecher11.slice(0, 5);
-            console.log(faecher11_0_5);
-        }
+        // if (index == 4) {
+        //     faecher11_0_5 = faecher11.slice(0, 5);
+        //     console.log(faecher11_0_5);
+        // }
     });
 });
 
@@ -44,16 +45,36 @@ let regel12 = false;
 
 // Regel 1
 // Die Bedingung "faecher11.length === 5" sollten wir ersetzen durch einen "fertig/bestaetigen" button der Eingabe
-const intervalId = setInterval(function() {
-    if (faecher11.length === 5) {
-        if (JSON.stringify(faecher11_0_5) === JSON.stringify(faecher12_0_5)) {
-            regel1 = true;
-        } else {
-            console.log("yeah");
-        }
-        clearInterval(intervalId); // Stop the interval
+// const intervalId = setInterval(function() {
+//     if (faecher11.length === 5) {
+//         if (JSON.stringify(faecher11_0_5) === JSON.stringify(faecher12_0_5)) {
+//             regel1 = true;
+//         } else {
+//             console.log("yeah");
+//         }
+//         clearInterval(intervalId); // Stop the interval
+//     }
+// }, 1000);
+
+inputFinishedButton.addEventListener('click', function() {
+    faecher11_0_4 = faecher11.slice(0, 4);
+    faecher11_0_5 = faecher11.slice(0, 5);
+
+
+    if (JSON.stringify(faecher11_0_5) === JSON.stringify(faecher12_0_5)) {
+        regel1 = true;
+    } else {
+        console.log("LK1, LK2, 3.-5.PK muessen in Jahren 11 und 12 gleich sein, nicht geschafft");
     }
-}, 1000);
+});
+
 
 // ------------------
 // Regel 2...
+
+
+// if (faecher11[0] === "De" || faecher11[0] === "Ma" || faecher11[0] === "Sp" || faecher11[0] === "En" || faecher11[0] === "Ph" || faecher11[0] === "Ch" || faecher11[0] === "Bi" || faecher11[1] === "De" || faecher11[1] === "Ma" || faecher11[1] === "Sp" || faecher11[1] === "Ee" || faecher11[1] === "Ph" || faecher11[1] === "Ch" || faecher11[1] === "Bi") {
+//     regel2 = true;
+// } else {
+//     console.log("Eines der folgenden Faecher:'De, Ma, Sp, En, Ph, Ch, Bi' muss ein LK sein");
+// }
