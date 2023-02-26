cf0f1af19d2c8e3490b0b2e04aaf26f55c386f1d
Lauri0604 <Laurenz.Karrasch@gmail.com>
2023-02-26 23:59:24 +0100

    rule 2 added

--- kursplaner.js
+++ kursplaner.js
@@ -2,7 +2,7 @@
 const faecher11 = [];
 let faecher11_0_4 = [];
 let faecher11_0_5 = [];
-const faecher12 = ['Englisch', 'Latein', 'Spanisch', 'Japanisch', 'Musik'];
+const faecher12 = ['Deutsch','Englisch', 'Latein', 'Spanisch', 'Japanisch', 'Musik'];
 let faecher12_0_5 = faecher12.slice(0, 5);
 
 const buttons = document.querySelectorAll('.subject-button');
@@ -59,22 +59,23 @@ let regel12 = false;
 inputFinishedButton.addEventListener('click', function() {
     faecher11_0_4 = faecher11.slice(0, 4);
     faecher11_0_5 = faecher11.slice(0, 5);
-
-
+    
+    
     if (JSON.stringify(faecher11_0_5) === JSON.stringify(faecher12_0_5)) {
         regel1 = true;
+        console.log("regel1 = true");
     } else {
         console.log("LK1, LK2, 3.-5.PK muessen in Jahren 11 und 12 gleich sein, nicht geschafft");
     }
+    
+    // ------------------
+    // Regel 2...
+    
+    if (faecher11[0] === "Deutsch" || faecher11[0] === "Mathematik" || faecher11[0] === "Spanisch" || faecher11[0] === "Englisch" || faecher11[0] === "Physik" || faecher11[0] === "Chemie" || faecher11[0] === "Biologie" || faecher11[1] === "Deutsch" || faecher11[1] === "Mathematik" || faecher11[1] === "Spanisch" || faecher11[1] === "Englisch" || faecher11[1] === "Physik" || faecher11[1] === "Chemie" || faecher11[1] === "Biologie") {
+        regel2 = true;
+        console.log("regel2 = true");
+    } else {
+        console.log("Eines der folgenden Faecher:'De, Ma, Sp, En, Ph, Ch, Bi' muss ein LK sein");
+    }
+    
 });
-
-
-// ------------------
-// Regel 2...
-
-
-// if (faecher11[0] === "De" || faecher11[0] === "Ma" || faecher11[0] === "Sp" || faecher11[0] === "En" || faecher11[0] === "Ph" || faecher11[0] === "Ch" || faecher11[0] === "Bi" || faecher11[1] === "De" || faecher11[1] === "Ma" || faecher11[1] === "Sp" || faecher11[1] === "Ee" || faecher11[1] === "Ph" || faecher11[1] === "Ch" || faecher11[1] === "Bi") {
-//     regel2 = true;
-// } else {
-//     console.log("Eines der folgenden Faecher:'De, Ma, Sp, En, Ph, Ch, Bi' muss ein LK sein");
-// }
